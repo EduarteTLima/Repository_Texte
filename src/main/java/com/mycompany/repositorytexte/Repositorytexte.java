18bdb75d79a00f7f883fb66f804fa3c077660245
EDUARDO TAVARES DE LIMA <eduardo.3946@aluno.pr.senac.br>
2023-09-11 11:10:21 -0300

    primeiro commit

---
+++ src/main/java/com/mycompany/repositorytexte/Repositorytexte.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.repositorytexte;
+
+/**
+ *
+ * @author eduardo.3946
+ */
+public class Repositorytexte {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
